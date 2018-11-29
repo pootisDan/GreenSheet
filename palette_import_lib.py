f7e8de31449d3404211ad634f8a05d5cacffc326
Dan <tim.tim>
2018-11-29 11:51:38 -0500

    filled in the code

--- palette_import_lib.py
+++ palette_import_lib.py
@@ -2,12 +2,19 @@
 functions to read the a CSV file
 and put it into a dictionary
 """
-
+import csv
 
 def read_palette_from_file(palette_filename):
-    """
-    read the palette from the csv file.
-    Each row is a key and an RGB triple.  Make that RGB triple
-    into a TUPLE and then put into a dictionary with the key.
-    """
-    pass
+	palette = dict()
+
+	with open(palette_filename) as palette_csv:
+		palette_reader = csv.reader(palette_csv)
+		for color in palette_reader:
+			# color0 = color[0]
+			# color1 = color[1]
+			# color2 = color[2]
+			# color3 = color[3]
+			#print(color0,color1,color2,color3)
+			palette[int(color[0])] = (int(color[1]),int(color[2]),int(color[3]))
+	return(palette)
+
